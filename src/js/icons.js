8053179b700204b6f65c21ffb24f0b28fdd8845e
Luis <166702635+LuisIannelloDev@users.noreply.github.com>
2024-06-26 11:12:46 +0000

    Simple Counter Luis

---
+++ src/js/icons.js
@@ -0,0 +1,9 @@
+// src/js/icons.js
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(faClock);
+
+//  * 5) Now, you can place the icon tag anywhere you want to icon to show, for example:
+<i className="fas fa-clock"></i>
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,59 @@
-//import react into the bundle
+// import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// import bootstrap and font-awesome
+import 'bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+import PropTypes from 'prop-types';
+
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+// function to create the counter component
+function SimpleCounter(props) {
+    return (
+        <div className="bigCounter">
+            <div className="calendar">
+                <FontAwesomeIcon icon={faClock} />
+            </div>
+            <div className="four">{props.digitFour}</div>
+            <div className="three">{props.digitThree}</div>
+            <div className="two">{props.digitTwo}</div>
+            <div className="one">{props.digitOne}</div>
+        </div>
+    );
+}
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+// define prop types for the SimpleCounter component
+SimpleCounter.propTypes = {
+    digitFour: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitOne: PropTypes.number
+}
 
+// get the root element
+const rootElement = document.getElementById('app');
+
+// create the root
+const root = ReactDOM.createRoot(rootElement);
+
+// counter variable
+let counter = 0;
+
+// setInterval function to update the counter every second
+setInterval(function() {
+    const four = Math.floor(counter / 1000);
+    const three = Math.floor(counter / 100) % 10;
+    const two = Math.floor(counter / 10) % 10;
+    const one = counter % 10;
+    counter++;
+
+    // render the SimpleCounter component with the updated digits
+    root.render(
+        <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />
+    );
+}, 1000);
